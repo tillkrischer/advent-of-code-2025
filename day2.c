9288c6f4181be9f345eafa462b6127754e39476b
Till Krischer <till.krischer@materna.de>
2025-12-04 18:58:31 +0100

    day 2.2

--- day2.c
+++ day2.c
@@ -25,27 +25,49 @@ int main(int argc, char *argv[]) {
     return 0;
 }
 
-bool is_repeat(unsigned long long n) {
-    int length = floor(log10(n)) + 1;
-    if (length % 2 == 1) {
+bool is_n_repeat(unsigned long long v, int n) {
+    int length = floor(log10(v)) + 1;
+    if (length % n != 0) {
         return false;
     }
-    unsigned long long divisor = pow(10, (int)(length / 2));
-    unsigned long long first = n / divisor;
-    unsigned long long second = n % divisor;
+    int part_length = length / n;
+    unsigned long long divisor = pow(10, part_length);
+    unsigned long long prev_part = v % divisor;
+    v /= divisor;
+    while (v > 0) {
+        unsigned long long part = v % divisor;
+        if (part != prev_part) {
+            return false;
+        }
+        prev_part = part;
+        v /= divisor;
+    }
+    return true;
+}
 
-    return first == second;
+bool is_any_repeat(unsigned long long v) {
+    int length = floor(log10(v)) + 1;
+    for (int parts = 2; parts <= length; parts++) {
+        if (is_n_repeat(v, parts)) {
+            return true;
+        }
+    }
+    return false;
 }
 
 void parse_file(FILE *f) {
-    unsigned long long start, end, count1 = 0;
+    unsigned long long start, end, count1 = 0, count2 = 0;
     do {
         fscanf(f, "%llu-%llu", &start, &end);
         for (unsigned long long i = start; i <= end; i++) {
-            if (is_repeat(i)) {
+            if (is_n_repeat(i, 2)) {
                 count1 += i;
+            }
+            if (is_any_repeat(i)) {
+                count2 += i;
             }
         }
     } while (fgetc(f) == ',');
     printf("%llu\n", count1);
+    printf("%llu\n", count2);
 }
