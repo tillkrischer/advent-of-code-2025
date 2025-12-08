7a3e0ceeb90687371dde24a50a3a2769371d4aa2
Till Krischer <till@krischer.io>
2025-12-08 21:51:12 +0100

    day 4.2

--- day4.c
+++ day4.c
@@ -4,6 +4,12 @@
 
 void parse_file(FILE *f);
 
+typedef struct {
+    char data[256][256];
+    int height;
+    int width;
+} Grid;
+
 int main(int argc, char *argv[]) {
     if (argc != 2) {
         fprintf(stderr, "Usage: %s <path>\n", argv[0]);
@@ -26,19 +32,19 @@ int main(int argc, char *argv[]) {
     return 0;
 }
 
-bool is_in_bounds(int H, int W, int y, int x) {
-    return (y >= 0 && y < H && x >= 0 && x < W);
+bool is_in_bounds(Grid *grid, int y, int x) {
+    return (y >= 0 && y < grid->height && x >= 0 && x < grid->width);
 }
 
-int count_adj(char line[256][256], int H, int W, int y, int x) {
+int count_adj(Grid *grid, int y, int x) {
     int count = 0;
     for (int dy = -1; dy <= 1; dy++) {
         for (int dx = -1; dx <= 1; dx++) {
             if (dy == 0 && dx == 0) {
                 continue;
             }
-            if (is_in_bounds(H, W, y + dy, x + dx) &&
-                line[y + dy][x + dx] == '@') {
+            if (is_in_bounds(grid, y + dy, x + dx) &&
+                grid->data[y + dy][x + dx] == '@') {
                 count += 1;
             }
         }
@@ -46,24 +52,40 @@ int count_adj(char line[256][256], int H, int W, int y, int x) {
     return count;
 }
 
-void parse_file(FILE *f) {
-    char line[256][256];
-    int H = 0, W = 0;
-    while (fgets(line[H], sizeof(line), f)) {
-        H += 1;
-    }
-    W = strlen(line[0]) - 1;
-
-    int count1 = 0;
-    for(int y = 0; y < H; y++) {
-        for(int x = 0; x < W; x++) {
-            if (line[y][x] == '@') {
-                int adj = count_adj(line, H, W, y, x);
+int remove_rolls(Grid *grid) {
+    Grid copy = *grid;
+    int removed = 0;
+    for (int y = 0; y < grid->height; y++) {
+        for (int x = 0; x < grid->width; x++) {
+            if (grid->data[y][x] == '@') {
+                int adj = count_adj(grid, y, x);
                 if (adj < 4) {
-                    count1 += 1;
+                    copy.data[y][x] = '.';
+                    removed += 1;
                 }
             }
         }
     }
+    *grid = copy;
+    return removed;
+}
+
+void parse_file(FILE *f) {
+    Grid grid = {0};
+    int row = 0;
+    while (fgets(grid.data[row], sizeof(grid.data), f)) {
+        row += 1;
+    }
+    grid.height = row;
+    grid.width = strlen(grid.data[0]) - 1;
+
+    int count1 = remove_rolls(&grid);
+    int count2 = count1;
+    int removed = 0;
+    while ((removed = remove_rolls(&grid)) > 0) {
+        count2 += removed;
+    }
+
     printf("%d\n", count1);
+    printf("%d\n", count2);
 }
